adb2bbeddf6dd844c03b7b74e1dfb3a2f8a400e6
tarmsrch159 <tarmsrch159@gmail.com>
2023-11-17 11:21:45 +0700

    commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -106,7 +106,7 @@ function Sidebar() {
 
                 <hr className="sidebar-divider d-none d-md-block" />
 
-                <NavLink to='https://rmutsb.vercel.app/' className={({ isActive }) => isActive ? ActiveLink : "nav-item"} style={{ textDecoration: "none" }}>
+                <NavLink to='https://frontend-user-test-deploy-fp9p.vercel.app/' className={({ isActive }) => isActive ? ActiveLink : "nav-item"} style={{ textDecoration: "none" }}>
                     <li >
                         <a className="nav-link">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-people-fill" viewBox="0 0 16 16">
